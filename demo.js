2b3b09998a71ea94b2ff57a96129c6d0fbba1eba
yejinzhang <1015451368@qq.com>
2021-07-03 14:51:02 +0800

    简易网站

---
+++ demo.js
@@ -0,0 +1,39 @@
+function init() {
+    bindEvent();
+    swiper();
+}
+init();
+
+function bindEvent() {
+    var bite = $('#swiper').width() / $('#swiper').height();
+    console.log(bite);
+    $('.btn').hover(function() {
+        $('.header ul.list').show();
+    })
+    $('.header ul.list').on('mouseleave', function() {
+        if (window.innerWidth <= 700) {
+            $(this).hide();
+        }
+    });
+    $(window).on('resize', function() {
+        var height = $('#swiper').width() / bite;
+        $('#swiper').css({ height: height + 'px' });
+        $('.content').css({ height: height + 'px' });
+        if (window.innerWidth > 700) {
+            $('.header ul.list').show();
+        }
+    });
+    $('.header a').click(function() {
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 20 + "px"
+        }, 500)
+        return false;
+    })
+}
+
+function swiper() {
+    $('#swiper').sliderImg({
+        image: ['./img/1.jpg', './img/2.jpg', './img/3.jpg'],
+        href: ['#', '#', '#']
+    });
+}
